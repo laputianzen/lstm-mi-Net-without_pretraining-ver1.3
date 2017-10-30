c830278c36fd875e100fdd2b64ac82ac777bfa54
Peter Tsai <laputianzen@gmail.com>
2017-10-30 15:31:42 +0800

    fix logit test txt filename

--- codes/miNet.py
+++ codes/miNet.py
@@ -601,7 +601,7 @@ def main_supervised(instNetList,num_inst,inputs,dataset,FLAGS):
             summary_writer.add_summary(summary_str, count)     
             
             Y_scaled, Y_unscaled = run_step(sess,[tf.nn.softmax(Y),Y],feed_dict)
-            np.savetxt('{}/logit{}.txt'.format(FLAGS._test_logit_txt,actual_epochs),Y_scaled,fmt='%.4f', delimiter=' ')
+            np.savetxt('{}/logit{}-test.txt'.format(FLAGS._test_logit_txt,actual_epochs),Y_scaled,fmt='%.4f', delimiter=' ')
             
             bagAccu,pAccu = metric.calculateAccu(Y_pred,inst_pred,test_multi_Y,test_multi_label,dataset)
             text_file.write('test bag accuracy %.5f, test inst accuracy %.5f\n\n' %(bagAccu, pAccu))
